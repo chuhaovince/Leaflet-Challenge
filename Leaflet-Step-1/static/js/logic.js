488d92c0e03738292c3fd47eb60bd182fb295c0b
Hao Chu <chuhaono.1@hotmail.com>
2020-02-02 04:59:34 -0500

    Add style for leaflet popups, create circles for each earthquake, used earthquake m value for the radius of the circles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 var mapConfig = {
-    center: [-115.5701667, 33.082],
-    zoom: 11
+    center: [37.09, -95.71],
+    zoom:4
 };
 
 var tileURL = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
@@ -8,7 +8,7 @@ var tileURL = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token
 var tileConfig = {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.street-basic",
+  id: "mapbox.streets-basic",
   accessToken: API_KEY
 };
 
@@ -19,11 +19,17 @@ var map = L.map("map", mapConfig);
 // Adding tile layer
 L.tileLayer(tileURL, tileConfig).addTo(map);
 
+
 // Loading geojson data
 d3.json(geoJsonURL).then(function(response) {
-    var geodata = L.choropleth(response, {
-
-        // Define what property in the features to use
-        valueProperty : ""
+    console.log(response.features[0].geometry.coordinates.slice(0,2))
+    response.features.forEach(location => 
+        L.circle(location.geometry.coordinates.slice(0,2).reverse(), {
+            fillOpacity : 0.75,
+            color : "red",
+            radius : location.properties.mag * 10000
     })
-})
+    .bindPopup(`<h3 style = {"text-align": "center"}><strong>${location.properties.place}</strong></h3><hr><h4 style = {"text-align": "center"}>M level: ${location.properties.mag}</h4><hr><h4 style = {"text-align": "center"}>Time: ${Date.parse(location.properties.time)}</h4>`)
+    .addTo(map)
+    )
+});
