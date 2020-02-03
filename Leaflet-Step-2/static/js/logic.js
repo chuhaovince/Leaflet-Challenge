fa6d160472b2c3cc223ed8ec4ec85c33c0d4b2ae
chuhaovince <53956182+chuhaovince@users.noreply.github.com>
2020-02-02 21:49:11 -0500

    Rearrange the logic and adding codes into differnt functions for calling

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,88 +1,105 @@
 var tectonicURL = "assets/tectonic_GeoJSON/PB2002_boundaries.json";
 var earthquake_geoJsonURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-var greyscalemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.light",
-  accessToken: API_KEY
-});
+function CreateMap(earthquake, tectonic) {
+  var mapConfig = {
+    center: [37.09, -95.71],
+    zoom:5,
+    types : [
+      satellitemap,
+      earthquake,
+      tectonic
+    ]
+  };
 
-var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.satellite",
-  accessToken: API_KEY
-});
+  var greyscalemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  });
 
-var outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-});
+  var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
 
+  var outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
 
-// Initialize all of the LayerGroups we'll be using
-// var layers = {
-//   greyscale : new L.layerGroup(),
-//   outdoors : new L.layerGroup(),
-//   satellite : new L.layerGroup()
-// };
+  // Create a basemap object to hold the greyscale layer
+  var basemap = {
+    "Greyscale" : greyscalemap,
+    "Outdoors" : outdoormap,
+    "Satellite" : satellitemap
+  };
 
-// Create a basemap object to hold the greyscale layer
-var basemap = {
-  "Greyscale" : greyscalemap,
-  "Outdoors" : outdoormap,
-  "Satellite" : satellitemap
+  // Create an overlaymap object to hold the earthquake and tectonic layers
+  var overlays = {
+    "Earthquakes" : earthquake,
+    "Fault lines" : tectonic
+  };
+  
+  // Create the default map with the satellite layers
+  var map = L.map("map", mapConfig);
+
+  // Pass our map layers into our layer control
+  // Add the layer control to the map
+  L.control.layers(basemap, overlays, {collapsed : false}).addTo(map);
+
+  // Adding legends
+  var legend = L.control({position : "bottomright"});
+  legend.onAdd = function() {
+      var div = L.DomUtil.create('div', 'info legend'),
+      mags = [0, 1, 2, 3, 4, 5],
+      labels = [];
+
+      // loop through colors
+      for (var i = 0; i < mags.length; i++) {
+          labels.push('<i class = "box" style="background:' + colorscale(mags[i]+1) + '"> </i>' +
+          mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+'))
+      };
+      div.innerHTML += labels.join("");
+
+      return div;
+  };
+  legend.addTo(map);
+
 };
+
+// Define color scales
+function colorscale(color) {
+  return color > 5  ?  "#c71e1e" :
+         color > 4  ?  "#ea822c" :
+         color > 3  ?  "#ee9c00" :
+         color > 2  ?  "#eecc00" :
+         color > 1  ?  "#b8d800" :
+                       "#98ee00";
+}
 
 // Create earthquake layergroup
 var earthquake = new L.layerGroup();
 //create tectonic layergroup
 var tectonic = new L.layerGroup();
 
-function MarkersAndLegend() {
-  // Loading earthquake geojson data and add the data to earthquake variable
-  d3.json(earthquake_geoJsonURL, function(response) {
-    response.features.forEach(location => 
-        L.circle(location.geometry.coordinates.slice(0,2).reverse(), {
-            fillOpacity : 0.8,
-            color : colorscale(location.properties.mag),
-            radius : location.properties.mag * 10000
-        })
-        .bindPopup(`<h3><strong>${location.properties.place}</strong></h3><hr><h4>M level: ${location.properties.mag}</h4><hr><h4>Time: ${new Date(location.properties.time)}</h4>`)
-    ).addTo(earthquake);
 
-      // Define color scales
-    function colorscale(color) {
-      return color > 5  ?  "#c71e1e" :
-            color > 4  ?  "#ea822c" :
-            color > 3  ?  "#ee9c00" :
-            color > 2  ?  "#eecc00" :
-            color > 1  ?  "#b8d800" :
-                          "#98ee00";
-    }
-
-    // Adding legends
-    var legend = L.control({position : "bottomright"});
-    legend.onAdd = function() {
-        var div = L.DomUtil.create('div', 'info legend'),
-        mags = [0, 1, 2, 3, 4, 5],
-        labels = [];
-
-        // loop through colors
-        for (var i = 0; i < mags.length; i++) {
-            labels.push('<i class = "box" style="background:' + colorscale(mags[i]+1) + '"> </i>' +
-            mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+'))
-        };
-        div.innerHTML += labels.join("");
-
-        return div;
-    };
-    legend.addTo(map);
-  });
-};
+// Loading earthquake geojson data and add the data to earthquake variable
+d3.json(earthquake_geoJsonURL, function(response) {
+  response.features.forEach(location => 
+      L.circle(location.geometry.coordinates.slice(0,2).reverse(), {
+          fillOpacity : 0.8,
+          color : colorscale(location.properties.mag),
+          radius : location.properties.mag * 10000
+      })
+      .bindPopup(`<h3><strong>${location.properties.place}</strong></h3><hr><h4>M level: ${location.properties.mag}</h4><hr><h4>Time: ${new Date(location.properties.time)}</h4>`)
+  ).addTo(earthquake);
+});
 
 // Load tectonic data and add to tectonic variable
 d3.json(tectonicURL, function(response) {
@@ -97,29 +114,4 @@ d3.json(tectonicURL, function(response) {
   }).addTo(tectonic)
 });
 
-MarkersAndLegend();
-
-var mapConfig = {
-  center: [37.09, -95.71],
-  zoom:5,
-  types : [
-    satellitemap,
-    earthquake,
-    tectonic
-  ]
-};
-
-// Create an overlaymap object to hold the earthquake and tectonic layers
-var overlays = {
-  "Earthquakes" : earthquake,
-  "Fault lines" : tectonic
-};
-
-// Create the default map with the satellite layers
-var map = L.map("map", mapConfig);
-
-
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(basemap, overlays, {collapsed : false}).addTo(map);
-
+CreateMap(earthquake, tectonic);
